a30fd011578376c56d7c5274d377a7c4f7a3b119
htchepannou <htchepannou@expedia.com>
2016-09-12 21:18:08 -0400

    deploy only master branch

--- after_success.sh
+++ after_success.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-wget http://roboconf.net/resources/build/settings.xml
-mvn clean deploy -q --settings settings.xml
+echo TRAVIS_BRANCH=$TRAVIS_BRANCH
+
+if ["$TRAVIS_BRANCH" = "master"]; then
+
+    wget http://roboconf.net/resources/build/settings.xml
+    mvn clean deploy -q --settings settings.xml
+
+fi
